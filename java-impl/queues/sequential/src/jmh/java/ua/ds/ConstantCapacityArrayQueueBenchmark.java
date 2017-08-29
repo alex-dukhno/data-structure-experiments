376869a3370efbc9c26985a5d4c78880d584b3b2
Alex-Diez <alex.dukhno@icloud.com>
2017-08-29 21:20:44 +0300

    [Java] made sequenctial queues benchmarks cleaner

--- java-impl/queues/sequential/src/jmh/java/ua/ds/ConstantCapacityArrayQueueBenchmark.java
+++ java-impl/queues/sequential/src/jmh/java/ua/ds/ConstantCapacityArrayQueueBenchmark.java
@@ -1,13 +1,9 @@
 package ua.ds;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-
-import java.util.stream.IntStream;
 
 //    Intel(R) Core(TM) i5-5257U CPU @ 2.70 GHz, 2 Core(s), 4 Logical Processor(s)
 //          cache sizes                 queue sizes
@@ -21,14 +17,14 @@ import java.util.stream.IntStream;
 //    L2   256KB      262144    32768     65536    131072
 //    L3     8MB     8388608  1048576   2097152   4194304
 @State(Scope.Benchmark)
-public class LinkedQueueBenchmark {
+public class ConstantCapacityArrayQueueBenchmark {
 
   @Param({"512", "1024", "2048", "4096", "8192", "16384", "32768", "65536", "131072", "262144", "524288", "1048576"})
   private int iterations;
 
   @Benchmark
-  public LinkedQueue enqueue() {
-    LinkedQueue queue = new LinkedQueue();
+  public NonResizableArrayQueuePrimitive enqueue_primitive() {
+    NonResizableArrayQueuePrimitive queue = new NonResizableArrayQueuePrimitive(iterations);
     for (int i = 0; i < iterations; i++) {
       queue.enqueue(i);
     }
@@ -36,8 +32,27 @@ public class LinkedQueueBenchmark {
   }
 
   @Benchmark
-  public int deque() {
-    LinkedQueue queue = enqueue();
+  public int deque_primitive() {
+    NonResizableArrayQueuePrimitive queue = enqueue_primitive();
+    int sum = 0;
+    for (int i = 0; i < iterations; i++) {
+      sum += queue.deque();
+    }
+    return sum;
+  }
+
+  @Benchmark
+  public NonResizableArrayQueueBoxed enqueue_boxed() {
+    NonResizableArrayQueueBoxed queue = new NonResizableArrayQueueBoxed(iterations);
+    for (int i = 0; i < iterations; i++) {
+      queue.enqueue(i);
+    }
+    return queue;
+  }
+
+  @Benchmark
+  public int deque_boxed() {
+    NonResizableArrayQueueBoxed queue = enqueue_boxed();
     int sum = 0;
     for (int i = 0; i < iterations; i++) {
       sum += queue.deque();
--- java-impl/run_banchmark.sh
+++ java-impl/run_banchmark.sh
@@ -3,13 +3,13 @@
 gradle clean jmhJar \
 && cd queues/sequential \
 && mkdir -p build/reports/jmh/linked/ \
-&& java -jar build/libs/sequential-1.0-jmh.jar -e 'BitAndResizableArrayQueueBenchmark|BranchResizableArrayQueueBenchmark|LinkedArrayQueueBenchmark|NonResizableArrayQueueBoxedBenchmark|NonResizableArrayQueuePrimitiveBenchmark' -f 1 -wi 20 -i 100 -gc true -tu us -bm ss -rf JSON -rff build/reports/jmh/linked/results.json \
+&& java -jar build/libs/sequential-1.0-jmh.jar -e 'ConstantCapacityArrayQueueBenchmark|LinkedArrayQueueBenchmark|ResizableArrayQueueBenchmark' -f 1 -wi 20 -i 100 -gc true -tu us -bm ss -rf JSON -rff build/reports/jmh/linked/results.json \
 && mkdir -p build/reports/jmh/non-resizable-array/ \
-&& java -jar build/libs/sequential-1.0-jmh.jar -e 'LinkedArrayQueueBenchmark|LinkedQueueBenchmark|BitAndResizableArrayQueueBenchmark|BranchResizableArrayQueueBenchmark' -f 1 -wi 10 -i 10 -tu ms -bm thrpt -rf JSON -rff build/reports/jmh/non-resizable-array/results.json \
+&& java -jar build/libs/sequential-1.0-jmh.jar -e 'LinkedArrayQueueBenchmark|LinkedQueueBenchmark|ResizableArrayQueueBenchmark' -f 1 -wi 10 -i 10 -tu ms -bm thrpt -rf JSON -rff build/reports/jmh/non-resizable-array/results.json \
 && mkdir -p build/reports/jmh/resizable-array/ \
-&& java -jar build/libs/sequential-1.0-jmh.jar -e 'LinkedArrayQueueBenchmark|LinkedQueueBenchmark|NonResizableArrayQueueBoxedBenchmark|NonResizableArrayQueuePrimitiveBenchmark' -f 1 -wi 10 -i 10 -tu ms -bm thrpt -rf JSON -rff build/reports/jmh/resizable-array/results.json \
+&& java -jar build/libs/sequential-1.0-jmh.jar -e 'LinkedArrayQueueBenchmark|LinkedQueueBenchmark|ConstantCapacityArrayQueueBenchmark' -f 1 -wi 10 -i 10 -tu ms -bm thrpt -rf JSON -rff build/reports/jmh/resizable-array/results.json \
 && mkdir -p build/reports/jmh/linked-array/ \
-&& java -jar build/libs/sequential-1.0-jmh.jar -e 'BitAndResizableArrayQueueBenchmark|BranchResizableArrayQueueBenchmark|LinkedQueueBenchmark|NonResizableArrayQueueBoxedBenchmark|NonResizableArrayQueuePrimitiveBenchmark' -bm thrpt -f 1 -wi 10 -i 10 -tu s -rf JSON -rff build/reports/jmh/linked-array/results.json \
+&& java -jar build/libs/sequential-1.0-jmh.jar -e 'ResizableArrayQueueBenchmark|LinkedQueueBenchmark|ConstantCapacityArrayQueueBenchmark' -bm thrpt -f 1 -wi 10 -i 10 -tu s -rf JSON -rff build/reports/jmh/linked-array/results.json \
 && mkdir -p build/reports/jmh/all/ \
 && java -jar build/libs/sequential-1.0-jmh.jar -bm thrpt -bm avgt -f 1 -wi 10 -i 10 -tu s -rf JSON -rff build/reports/jmh/all/results.json \
 && cd ../.. \
